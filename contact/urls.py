dc984f712fd277c8326e6a380b4f5637bde9e9d9
seyed <seyedmo30@gmail.com>
2019-02-18 16:37:55 +0330

    fffffffiiinish

--- contact/urls.py
+++ contact/urls.py
@@ -7,11 +7,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/<int:grp_id>/',views.contacts ,name='contacts'),
+    path('contacts/', views.contacts_all,name='contacts_all'),
+    path('contacts/<int:grp_id>/delete/<int:contact_id>/',views.delete ,name='delete'),
+    path('contacts//delete/<int:contact_id>/',views.delete_a ,name='delete_a'),
     path('signup/', views.signup,name='signup'),
     path('login/', views.login,name='login'),
     path('logout/', views.logout,name='logout'),
     path('add/', views.add,name='add'),
+    path('contacts/update/<int:contact_id>/',views.update ,name='update'),
     path('add_grp/', views.add_grp,name='add_grp'),
-    path('contacts/', views.contacts_all,name='contacts_all'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- phone/views.py
+++ phone/views.py
@@ -9,7 +9,42 @@ def contacts(request,grp_id):
         group_id=get_object_or_404(Grp,pk=grp_id)
         groups=Grp.objects.all().filter(user=request.user)
         contacts=Phone.objects.all().filter(user=request.user).filter(group=group_id)
-        return render(request,'phone/index.html',{'contacts':contacts,'groups':groups})
+        return render(request,'phone/index.html',{'contacts':contacts,'groups':groups,'grp_id':group_id.id})
+
+
+
+@login_required(login_url="signup")
+def update(request,contact_id):
+    if request.method=='POST':
+            contact=get_object_or_404(Phone,pk=contact_id)
+            if request.POST['name'] and request.POST['number'] and request.POST['group']  :
+
+                group=Grp.objects.get(id=request.POST['group'])
+                Phone.objects.filter(pk=contact_id).update(name=request.POST['name'],number=request.POST['number'],group=group)
+                return redirect('/contacts/')
+            else:
+                groups=Grp.objects.all().filter(user=request.user)
+                return render(request,'phone/add.html',{'error':'All field are required','groups':groups})
+
+
+    else:
+            contact=get_object_or_404(Phone,pk=contact_id)
+            groups=Grp.objects.all().filter(user=request.user)
+            return render(request,'phone/update.html',{'groups':groups,'contact':contact})
+
+
+
+@login_required(login_url="signup")
+def delete(request,contact_id,grp_id):
+        contact=get_object_or_404(Phone,pk=contact_id)
+        Phone.objects.filter(id=contact.id).delete()
+        return redirect('/contacts/'+str(grp_id)+'/')
+
+@login_required(login_url="signup")
+def delete_a(request,contact_id):
+        contact=get_object_or_404(Phone,pk=contact_id)
+        Phone.objects.filter(id=contact.id).delete()
+        return redirect('/contacts/')
 
 @login_required(login_url="signup")
 def contacts_all(request):
@@ -41,13 +76,11 @@ def add(request):
 
     if request.method=='POST':
         grps=Grp.objects.all().filter(user=request.user)
-        print(grps)
-
         if request.POST['name'] and request.POST['number'] and request.POST['group']  :
             phone=Phone()
             phone.name=request.POST['name']
             phone.number=request.POST['number']
-            phone.group=request.POST ['group']
+            phone.group=Grp.objects.get(id=request.POST['group'])
             phone.user=request.user
             phone.save()
             return redirect('/contacts/')
@@ -65,25 +98,20 @@ def add(request):
 def add_grp(request):
 
     if request.method=='POST':
-        grps=Grp.objects.all().filter(user=request.user)
-        print(grps)
-
-        if request.POST['name'] and request.POST['number'] and request.POST['group']  :
-            phone=Phone()
-            phone.name=request.POST['name']
-            phone.number=request.POST['number']
-            phone.group=request.POST ['group']
-            phone.user=request.user
-            phone.save()
+        if request.POST['name']  :
+            grp=Grp()
+            grp.name=request.POST['name']
+            grp.user=request.user
+            grp.save()
             return redirect('/contacts/')
         else:
 
             groups=Grp.objects.all().filter(user=request.user)
-            return render(request,'phone/add.html',{'error':'All field are required','groups':groups})
+            return render(request,'phone/add_grp.html',{'error':'All field are required','groups':groups})
 
     else:
         groups=Grp.objects.all().filter(user=request.user)
-        return render(request,'phone/add.html',{'groups':groups})
+        return render(request,'phone/add_grp.html',{'groups':groups})
 
 
 def login (request):
